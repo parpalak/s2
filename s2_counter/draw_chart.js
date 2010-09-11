a5cace3cf63cae771055835b077d22c22da48d42
Roman <roman@written.ru>
2010-09-11 10:52:32 +0000

    s2_counter: Prepared for extensibility. Updated charts appearance.

--- s2_counter/draw_chart.js
+++ s2_counter/draw_chart.js
@@ -12,17 +12,19 @@ function s2_counter_draw_chart ()
 {
 	var hits = new SWFObject("../extensions/s2_counter/amstock/amstock.swf", "amstock", "100%", "400px", "8", "#FFFFFF");
 	hits.addVariable("path", "../extensions/s2_counter/");
-	hits.addVariable("settings_file", encodeURIComponent("../extensions/s2_counter/traffic.xml?" + Math.random()));
+	var settings_file = "../extensions/s2_counter/traffic.xml?" + Math.random();
 
 	(hook = hooks['fn_s2_counter_draw_chart_pre_hits']) ? eval(hook) : null;
 
+	hits.addVariable("settings_file", encodeURIComponent(settings_file));
 	hits.write("s2_counter_hits");
 
 	var rss = new SWFObject("../extensions/s2_counter/amstock/amstock.swf", "amstock", "100%", "400px", "8", "#FFFFFF");
 	rss.addVariable("path", "../extensions/s2_counter/");
-	rss.addVariable("settings_file", encodeURIComponent("../extensions/s2_counter/rss.xml?" + Math.random()));
+	settings_file = "../extensions/s2_counter/rss.xml?" + Math.random();
 
 	(hook = hooks['fn_s2_counter_draw_chart_pre_rss']) ? eval(hook) : null;
 
+	rss.addVariable("settings_file", encodeURIComponent(settings_file));
 	rss.write("s2_counter_rss");
 }
