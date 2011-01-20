5635577233450f0542f2234f8111c9b32f30db42
Roman <roman@written.ru>
2011-01-20 20:16:35 +0000

    s2_counter: Now graphs are built only once.

--- s2_counter/draw_chart.js
+++ s2_counter/draw_chart.js
@@ -8,23 +8,36 @@
  * @package s2_counter
  */
 
-function s2_counter_draw_chart ()
-{
-	var hits = new SWFObject("../_extensions/s2_counter/amstock/amstock.swf", "amstock", "100%", "400px", "8", "#FFFFFF");
-	hits.addVariable("path", "../_extensions/s2_counter/");
-	var settings_file = "../_extensions/s2_counter/traffic.xml?" + Math.random();
 
-	(hook = hooks['fn_s2_counter_draw_chart_pre_hits']) ? eval(hook) : null;
+var s2_counter = {
 
-	hits.addVariable("settings_file", encodeURIComponent(settings_file));
-	hits.write("s2_counter_hits");
+	hits: null,
+	rss: null,
+	draw_chart: function ()
+	{
+		if (!s2_counter.hits)
+		{
+			s2_counter.hits = new SWFObject("../_extensions/s2_counter/amstock/amstock.swf", "amstock", "100%", "400px", "8", "#FFFFFF");
+			s2_counter.hits.addVariable("path", "../_extensions/s2_counter/");
+			var settings_file = "../_extensions/s2_counter/traffic.xml?" + Math.random();
 
-	var rss = new SWFObject("../_extensions/s2_counter/amstock/amstock.swf", "amstock", "100%", "400px", "8", "#FFFFFF");
-	rss.addVariable("path", "../_extensions/s2_counter/");
-	settings_file = "../_extensions/s2_counter/rss.xml?" + Math.random();
+			(hook = hooks['fn_s2_counter_draw_chart_pre_hits']) ? eval(hook) : null;
 
-	(hook = hooks['fn_s2_counter_draw_chart_pre_rss']) ? eval(hook) : null;
+			s2_counter.hits.addVariable("settings_file", encodeURIComponent(settings_file));
+		}
+		s2_counter.hits.write("s2_counter_hits");
 
-	rss.addVariable("settings_file", encodeURIComponent(settings_file));
-	rss.write("s2_counter_rss");
+		if (!s2_counter.rss)
+		{
+			s2_counter.rss = new SWFObject("../_extensions/s2_counter/amstock/amstock.swf", "amstock", "100%", "400px", "8", "#FFFFFF");
+			s2_counter.rss.addVariable("path", "../_extensions/s2_counter/");
+			settings_file = "../_extensions/s2_counter/rss.xml?" + Math.random();
+
+			(hook = hooks['fn_s2_counter_draw_chart_pre_rss']) ? eval(hook) : null;
+
+			s2_counter.rss.addVariable("settings_file", encodeURIComponent(settings_file));
+		}
+		s2_counter.rss.write("s2_counter_rss");
+	},
+
 }
