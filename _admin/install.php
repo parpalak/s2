e6a9332f24902e9618e64ff6f249f68a208be9fb
Roman <roman@written.ru>
2011-02-11 18:20:15 +0000

    Fixed an error in install.php (introduced in [369]).

--- _admin/install.php
+++ _admin/install.php
@@ -612,7 +612,7 @@ else
 		'INDEXES'		=> array(
 			'url_idx'			=> array('url'),
 			'create_time_idx'	=> array('create_time'),
-			'parent_id_idx'		=> array('parent_id')
+			'parent_id_idx'		=> array('parent_id'),
 			'template_idx'		=> array('template')
 		)
 	);
