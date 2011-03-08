744042064721d7c3b0536255f5bab25c4f0d948f
Roman <roman@written.ru>
2011-03-08 14:22:54 +0000

    s2_search: Added missing X-Powered-By header.

--- s2_search/ajax.php
+++ s2_search/ajax.php
@@ -13,6 +13,8 @@ header('Content-Type: text/html; charset=utf-8');
 define('S2_ROOT', '../../');
 require(S2_ROOT.'_include/common.php');
 
+header('X-Powered-By: S2/'.S2_VERSION);
+
 $s2_search_query = isset($_GET['q']) ? $_GET['q'] : '';
 
 require 'stemmer.class.php';
