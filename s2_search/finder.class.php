65472fcc32a94c6c620ba7c8f887bf52008b8bcf
Roman <roman@written.ru>
2011-05-03 09:16:05 +0000

    s2_search: Fixed a buggy URL formatting when indexing changed paged.

--- s2_search/finder.class.php
+++ s2_search/finder.class.php
@@ -376,7 +376,7 @@ class s2_search_finder
 				'title'		=> $article['title'],
 				'descr'		=> $article['meta_desc'],
 				'time'		=> $article['create_time'],
-				'url'		=> S2_BASE_URL.$parent_path.'/'.urlencode($article['url']).($article['url'] && $article['is_children'] ? '/' : ''),
+				'url'		=> $parent_path.'/'.urlencode($article['url']).($article['url'] && $article['is_children'] ? '/' : ''),
 			)
 		);
 
