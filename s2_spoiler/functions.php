9166f729adabc93ff4833167e2363de42fed9cc6
Roman <roman@written.ru>
2011-05-08 07:57:12 +0000

    s2_spoiler: Added animation effects.

--- s2_spoiler/functions.php
+++ s2_spoiler/functions.php
@@ -57,24 +57,61 @@ function s2_spoiler_make ($contents, $soft = 0)
 		ob_start();
 ?>
 <script type="text/javascript">
+function s2_spoiler_get_height (eItem)
+{
+	var rect = eItem.getBoundingClientRect();
+	return rect ? (rect.bottom - rect.top) : eItem.offsetHeight;
+}
+
+function s2_spoiler_animate (eItem, sProperty, from, to, duration, fFinalize)
+{
+	var start = new Date().getTime();
+	setTimeout(function() {
+		var progress = ((new Date().getTime()) - start) / duration;
+		eItem.style['height'] = parseInt((to - from) * 0.5 * (1 - Math.cos(progress * Math.PI)) + from) + "px";
+		if (progress < 1)
+			setTimeout(arguments.callee, 10);
+		else
+			fFinalize();
+	}, 10);
+}
+
 function s2_spoiler_flip (eItem)
 {
+	var eParent = eItem.parentNode;
 	if (eItem.className == 's2_spoiler_head')
 	{
 		eItem.className = 's2_spoiler_head_expand';
+		var from = s2_spoiler_get_height(eItem);
+		eParent.style.height = from + 'px';
+		eParent.style.overflow = 'hidden';
 		eItem.nextSibling.style.display = 'block';
+		var to = from + s2_spoiler_get_height(eItem.nextSibling);
+		var finalize = function ()
+		{
+			eParent.style.height = eParent.style.overflow = 'auto';
+		}
 	}
 	else
 	{
 		eItem.className = 's2_spoiler_head';
-		eItem.nextSibling.style.display = 'none';
+		var from = s2_spoiler_get_height(eParent);
+		var to = s2_spoiler_get_height(eItem);
+		eParent.style.height = from + 'px';
+		eParent.style.overflow = 'hidden';
+		var finalize = function ()
+		{
+			eItem.nextSibling.style.display = 'none';
+			eParent.style.height = eParent.style.overflow = 'auto';
+		}
 	}
+	s2_spoiler_animate(eParent, 'height', from, to, 200, finalize);
 }
 
 (function () {
 	var head = document.getElementsByTagName('head')[0],
 		style = document.createElement('style'),
-		rules = '.s2_spoiler {margin: 1em 0; } .s2_spoiler_body {display: none;} .s2_spoiler_head, .s2_spoiler_head_expand {display: inline-block; border-bottom: 1px dashed; cursor: pointer;} .s2_spoiler_head:before, .s2_spoiler_head_expand:before {position: absolute; margin-left: -12px; margin-top: 6px; display: inline-block; content: "+"; border: 1px dotted #000; color: #000; line-height: 8px; font-size: 14px; width: 8px; text-align: center; cursor: pointer; } .s2_spoiler_head_expand:before {content: \'\\2212\'}';
+		rules = '.s2_spoiler {margin: 1em 0;} .s2_spoiler_body {display: none;} .s2_spoiler_head, .s2_spoiler_head_expand {display: inline-block; border-bottom: 1px dashed; cursor: pointer;} .s2_spoiler_head:before, .s2_spoiler_head_expand:before {position: absolute; margin-left: -14px; margin-top: 6px; display: inline-block; content: "+"; border: 1px solid #999; color: #999; background: #fff; line-height: 8px; font-size: 14px; width: 8px; text-align: center; cursor: pointer; } .s2_spoiler_head_expand:before {content: \'\\2212\'}';
 
 	style.type = 'text/css';
 	if (style.styleSheet)
