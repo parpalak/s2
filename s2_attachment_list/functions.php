8b5994b4b96847d9dc90becf817942596df6edce
Roman <roman@written.ru>
2011-05-17 06:15:03 +0000

    s2_attachment_list: Fixed pages URL in the attachment list.

--- s2_attachment_list/functions.php
+++ s2_attachment_list/functions.php
@@ -19,7 +19,7 @@ function s2_attachment_paging ($page, $total_pages, $current_path)
 	return '<div class="paging">'.$str.'</div>';
 }
 
-function s2_attachment_list ($id, $parent_path, $current_path, $config, $page_limit)
+function s2_attachment_list ($id, $current_path, $config, $page_limit)
 {
 	global $s2_db, $lang_s2_attachment;
 
@@ -107,7 +107,7 @@ function s2_attachment_list ($id, $parent_path, $current_path, $config, $page_li
 			if ($conf_name == 'files')
 				$item = isset($files[$row['id']]) ? implode('<br />', $files[$row['id']]) : '';
 			elseif ($conf_name == 'title')
-				$item = '<a href="'.S2_PATH.S2_URL_PREFIX.$parent_path.'/'.urlencode($row['url']).($row['children_exist'] ? '/' : '').'">'.$row['title'].'</a>';
+				$item = '<a href="'.S2_PATH.S2_URL_PREFIX.$current_path.'/'.urlencode($row['url']).($row['children_exist'] ? '/' : '').'">'.$row['title'].'</a>';
 			elseif ($conf_name == 'create_time' || $conf_name == 'modify_time')
 				$item = $row[$conf_name] ? '<nobr>'.s2_date($row[$conf_name]).'</nobr>' : '';
 			else
