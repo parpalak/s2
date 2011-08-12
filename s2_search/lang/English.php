71a8b7ec78d4913ef5db4dd5c0f9fd6b87228cc5
Roman <roman@written.ru>
2011-08-12 09:02:42 +0000

    s2_search: Added English translation.

---
+++ s2_search/lang/English.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang_s2_search = array(
+
+'Search'					=> 'Search',
+'Search button'				=> 'Search',
+
+'Quick search'				=> 'Quick search',
+'Quick search label'		=> 'Show suggestions based on the search over titles while typing.',
+
+'Found'						=> 'Pages found: %d',
+'Not found'					=> 'Your search did not match any documents.',
+
+'Crumbs'					=> '<a href="%2$s">%1$s</a> &rarr; %3$s',
+'Reindex'					=> 'reindex',
+'Reindex title'				=> 'May take much time. You can run it again in case of connection loss.',
+'Info link'					=> 'Search index: %1$s (%2$s)',
+
+);
