1399203d6f0aee82c08c4d2937a049374480eaf3
Roman <roman@written.ru>
2011-08-21 10:19:45 +0000

    Added new extension s2_seo.

---
+++ s2_seo/lang/English.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang_s2_seo = array(
+
+'Title help'				=> 'Content of the &lt;title&gt; tag',
+'Title'						=> 'HTML title',
+
+);
---
+++ s2_seo/lang/Russian.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang_s2_seo = array(
+
+'Title help'				=> 'Содержимое тега &lt;title&gt;',
+'Title'						=> 'HTML title',
+
+);
