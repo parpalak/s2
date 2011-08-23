58c84c23ea4d5c87e0753db30bde6d3bc9159608
Roman <roman@written.ru>
2011-08-23 20:17:10 +0000

    s2_blog: Changed unsubscribe code generation.

--- s2_blog/blog_lib.php
+++ s2_blog/blog_lib.php
@@ -473,7 +473,7 @@ function s2_blog_toggle_hide_comment ($id)
 
 			foreach ($receivers as $receiver)
 			{
-				$unsubscribe_link = S2_BASE_URL.'/comment.php?mail='.urlencode($receiver['email']).'&id='.$comment['post_id'].'&blog_unsubscribe='.substr(md5($receiver['id'].$receiver['ip'].$receiver['nick'].$receiver['email'].$receiver['time']), 0, 16);
+				$unsubscribe_link = S2_BASE_URL.'/comment.php?mail='.urlencode($receiver['email']).'&id='.$comment['post_id'].'&blog_unsubscribe='.base_convert(substr(md5($receiver['id'].$receiver['ip'].$receiver['nick'].$receiver['email'].$receiver['time']), 0, 16), 16, 36);
 				s2_mail_comment($receiver['nick'], $receiver['email'], $comment['text'], $post['title'], $link, $comment['nick'], $unsubscribe_link);
 			}
 		}
