bb5dc4474da4101e7efa348916d3b51060239693
Roman <roman@written.ru>
2011-09-29 09:17:39 +0000

    s2_latex: Cache adjusting.

--- s2_latex/latex.php
+++ s2_latex/latex.php
@@ -14,19 +14,19 @@ require S2_ROOT.'_include/common.php';
 
 header('X-Powered-By: S2/'.S2_VERSION);
 
-$formula = isset($_GET['latex']) ? trim($_GET['latex']) : '';
+$formula = isset($_GET['latex']) ? trim((string) $_GET['latex']) : '';
 $format = (isset($_GET['type']) && $_GET['type'] == 'svg' ) ? 'svg' : 'gif';
 
 $hash = md5($formula);
 $filename = 'cache/'.$hash.'.'.$format;
 if (is_file($filename))
 {
-/*	$mtime = filemtime($filename);
+	$mtime = filemtime($filename);
  	if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && $mtime <= strtotime(substr($_SERVER['HTTP_IF_MODIFIED_SINCE'], 5)))
 	{
 		header('HTTP/1.1 304 Not Modified');
 		die;
-	} */
+	}
 	$file = file_get_contents($filename);
 }
 else
@@ -35,14 +35,17 @@ else
 	if (!$result)
 		die('latex.codecogs.com is unavailable.');
 	$file = $result['content'];
-	$file = str_replace('<script type="text/ecmascript" xlink:href="http://codecogs.izyba.com/svg.js"/>', '', $file);
+	if ($format == 'svg')
+		$file = str_replace('<script type="text/ecmascript" xlink:href="http://codecogs.izyba.com/svg.js"/>', '', $file);
+
 	file_put_contents($filename, $file);
-//	$mtime = time();
+	$mtime = time();
+	clearstatcache();
 }
 
 header('Cache-Control: public,max-age=3600');
 header('Expires: '.gmdate('D, d M Y H:i:s', time() + 3600) . ' GMT');
-//header('Last-Modified: '.gmdate('D, d M Y H:i:s', $mtime).' GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s', $mtime).' GMT');
 header($format == 'svg' ? 'Content-type: image/svg+xml' : 'Content-type: image/gif');
 header('Content-Length: '.strlen($file));
 
