be4ea91cf7a09056a388537a597303d3697717be
Roman <roman@written.ru>
2012-01-08 09:54:18 +0000

    s2_search: Trying to optimize main regex of snippets formatting.

--- s2_search/finder.class.php
+++ s2_search/finder.class.php
@@ -619,8 +619,7 @@ class s2_search_finder extends s2_search_worker
 			}
 
 			// Check the text for the query words
-			// Modifier S works poorly on cyrillic :(
-			preg_match_all('#(?<=[^a-zа-я]|^)('.implode('|', $stems).')[a-zа-я]*#sui', $string, $matches, PREG_OFFSET_CAPTURE);
+			preg_match_all('#(?<=\s|^)('.implode('|', $stems).')[^\s\.?!:;,()]*#Ssi', $string, $matches, PREG_OFFSET_CAPTURE);
 
 			$line_num = 0;
 			$line_end = strlen($lines[$line_num]);
