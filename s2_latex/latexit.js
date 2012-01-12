6521cddd9eec97968dcd5fa46eacbdd4c0e7779f
Roman <roman@written.ru>
2012-01-12 13:05:04 +0000

    s2_latex: Removed unused code.

--- s2_latex/latexit.js
+++ s2_latex/latexit.js
@@ -4,7 +4,9 @@
 * Modified by Roman Parpalak
 */
 
-var LatexIT = (function ()
+var LatexIT = {scale : function(e,scale) {}};
+
+(function ()
 {
 	var verOffset = navigator.userAgent.indexOf('Firefox'),
 		ext = 'gif',
@@ -37,172 +39,123 @@ var LatexIT = (function ()
 		style.appendChild(document.createTextNode(rules));
 	head.appendChild(style);
 
-	return ({
-		mode : ext,
+	function bindReady (handler)
+	{
+		var called = false;
 
-		pre : function(txt)
+		function ready() 
 		{
-			if (!txt.match(/<img.*?>/i))
+			if (called)
+				return;
+			called = true;
+			handler();
+		}
+
+		if (document.addEventListener)
+		{
+			document.addEventListener("DOMContentLoaded", function()
 			{
-				//Clean code
-				txt = txt.replace(/<br>/gi,"");
-				txt = txt.replace(/<br \/>/gi,"");
-
-				//Create img tag
-				txt = "<img src=\"http://latex.codecogs.com/" + this.mode + ".latex?" + txt + "\" alt=\"" + txt + "\" title=\"" + txt + "\" border=\"0\" class=\"latex\" style=\"margin:0; padding:0; border:0\" />";
-			}
-			return txt;
-		},
-
-		create_image : function (sText)
+				document.removeEventListener("DOMContentLoaded", arguments.callee, false);
+				ready();
+			}, false );
+		}
+		else if (document.attachEvent)
 		{
-			sText = sText.replace(/\$\$(.*)\$\$/g, '$1');
-
-			var eImg = document.createElement('IMG');
-			eImg.setAttribute('src', s2_latex_url + '/latex.php?type=' + this.mode + '&latex=' + encodeURIComponent(sText));
-			eImg.setAttribute('alt', sText);
-			eImg.setAttribute('border', '0');
-			eImg.className = 's2_latex';
-
-			// This outer span fixes some bugs in Opera
-			var eSpan = document.createElement('SPAN');
-			eSpan.className = 's2_latex_span';
-			eSpan.appendChild(eImg);
-
-			return eSpan;
-		},
-
-		process_item : function (eItem)
-		{
-			var eNextChild = eItem.firstChild;
-
-			while (eNextChild)
+			if (document.documentElement.doScroll && window == window.top)
 			{
-				var eCurChild = eNextChild;
-				eNextChild = eNextChild.nextSibling;
-
-				if (eCurChild.nodeType == 1 && eCurChild.nodeName != 'SCRIPT' && eCurChild.nodeName != 'TEXTAREA' && eCurChild.nodeName != 'OBJECT')
-					LatexIT.process_item(eCurChild);
-				else if (eCurChild.nodeType == 3)
+				function tryScroll()
 				{
-					var as = (' ' + eCurChild.nodeValue + ' ').split(/\$\$/g);
-					var item_num = as.length;
-					if (item_num > 2)
+					if (called)
+						return;
+					try 
 					{
-						as[0] = as[0].substring(1);
-						as[item_num - 1] = as[item_num - 1].substring(0, as[item_num - 1].length - 1);
-
-						for (var i = 0; i < item_num; i++)
-						{
-							if (i % 2)
-							{
-								if (i + 1 < item_num)
-									eItem.insertBefore(LatexIT.create_image(as[i]), eCurChild);
-								else
-									eItem.insertBefore(document.createTextNode('$$' + as[i]), eCurChild);
-							}
-							else
-								eItem.insertBefore(document.createTextNode(as[i]), eCurChild);
-						}
-
-						eItem.removeChild(eCurChild);
-					}
-				}
-			}
-		},
-
-		render : function(tag, latexmode)
-		{
-			var aeItem = document.getElementsByTagName(tag);
-			for (var i = aeItem.length; i-- ;)
-			{
-				var eItem = aeItem[i];
-				if (latexmode)
-					LatexIT.process_item(eItem);
-				else
-				{
-					try
-					{
-						if (eItem.getAttribute("lang") == "latex" || eItem.getAttribute("xml:lang") == "latex")
-							eItem.innerHTML = LatexIT.pre(eItem.innerHTML);
-					}
-					catch (e) {}
-				}
-			} 
-		},
-
-		add : function(tag, latexmode)
-		{
-			if (typeof(latexmode) == 'undefined')
-				latexmode = false;
-			LatexIT.bindReady(function()
-			{
-				LatexIT.render(tag, latexmode);
-			});
-		},
-
-		bindReady : function (handler)
-		{
-			var called = false;
-
-			function ready() 
-			{
-				if (called)
-					return;
-				called = true;
-				handler();
-			}
-
-			if (document.addEventListener)
-			{
-				document.addEventListener("DOMContentLoaded", function()
-				{
-					document.removeEventListener("DOMContentLoaded", arguments.callee, false);
-					ready();
-				}, false );
-			}
-			else if (document.attachEvent)
-			{
-				if (document.documentElement.doScroll && window == window.top)
-				{
-					function tryScroll()
-					{
-						if (called)
-							return;
-						try 
-						{
-							document.documentElement.doScroll("left");
-							ready();
-						}
-						catch(e)
-						{
-							setTimeout(tryScroll, 0);
-						}
-					}
-					tryScroll();
-				}
-
-				document.attachEvent("onreadystatechange", function()
-				{
-					if (document.readyState === "complete" )
-					{
-						document.detachEvent("onreadystatechange", arguments.callee);
+						document.documentElement.doScroll("left");
 						ready();
 					}
-				});
+					catch(e)
+					{
+						setTimeout(tryScroll, 0);
+					}
+				}
+				tryScroll();
 			}
 
-			if (window.addEventListener)
-				window.addEventListener('load', ready, false)
-			else if (window.attachEvent)
-				window.attachEvent('onload', ready)
-		},
-
-		scale : function(e,scale)
-		{
+			document.attachEvent("onreadystatechange", function()
+			{
+				if (document.readyState === "complete" )
+				{
+					document.detachEvent("onreadystatechange", arguments.callee);
+					ready();
+				}
+			});
 		}
+
+		if (window.addEventListener)
+			window.addEventListener('load', ready, false)
+		else if (window.attachEvent)
+			window.attachEvent('onload', ready)
+	}
+
+	function create_image (sText)
+	{
+		sText = sText.replace(/\$\$(.*)\$\$/g, '$1');
+
+		var eImg = document.createElement('IMG');
+		eImg.setAttribute('src', s2_latex_url + '/latex.php?type=' + ext + '&latex=' + encodeURIComponent(sText));
+		eImg.setAttribute('alt', sText);
+		eImg.setAttribute('border', '0');
+		eImg.className = 's2_latex';
+
+		// This outer span fixes some bugs in Opera
+		var eSpan = document.createElement('SPAN');
+		eSpan.className = 's2_latex_span';
+		eSpan.appendChild(eImg);
+
+		return eSpan;
+	}
+
+	// Walks through the DOM tree
+	function process_item (eItem)
+	{
+		var eNextChild = eItem.firstChild;
+
+		while (eNextChild)
+		{
+			var eCurChild = eNextChild;
+			eNextChild = eNextChild.nextSibling;
+
+			if (eCurChild.nodeType == 1 && eCurChild.nodeName != 'SCRIPT' && eCurChild.nodeName != 'TEXTAREA' && eCurChild.nodeName != 'OBJECT')
+				process_item(eCurChild);
+			else if (eCurChild.nodeType == 3)
+			{
+				var as = (' ' + eCurChild.nodeValue + ' ').split(/\$\$/g);
+				var item_num = as.length;
+				if (item_num > 2)
+				{
+					as[0] = as[0].substring(1);
+					as[item_num - 1] = as[item_num - 1].substring(0, as[item_num - 1].length - 1);
+
+					for (var i = 0; i < item_num; i++)
+					{
+						if (i % 2)
+						{
+							if (i + 1 < item_num)
+								eItem.insertBefore(create_image(as[i]), eCurChild);
+							else
+								eItem.insertBefore(document.createTextNode('$$' + as[i]), eCurChild);
+						}
+						else
+							eItem.insertBefore(document.createTextNode(as[i]), eCurChild);
+					}
+
+					eItem.removeChild(eCurChild);
+				}
+			}
+		}
+	}
+
+	bindReady(function()
+	{
+		process_item(document.getElementsByTagName('body')[0]);
 	});
 })();
-
-LatexIT.add('*');
-LatexIT.add('body', true);
