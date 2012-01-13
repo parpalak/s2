5f4d302851dc24304148641da4e393891d8d7a94
Roman <roman@written.ru>
2012-01-13 22:11:15 +0000

    s2_search: Changed chapter id building.

--- s2_search/admin.js
+++ s2_search/admin.js
@@ -38,10 +38,10 @@ var s2_search = {
 		}
 	},
 
-	refresh_index: function (sChapter)
+	refresh_index: function (sAction, sId)
 	{
-		GETAsyncRequest(sUrl + 'action=s2_search_makeindex&chapter=' + encodeURIComponent(sChapter));
+		GETAsyncRequest(sUrl + 'action=s2_search_makeindex&save_action=' + encodeURIComponent(sAction) + '&id=' + encodeURIComponent(sId));
 	}
 }
 
-Hooks.add('fn_save_article_end', 's2_search.refresh_index((sAction == "save_blog" ? "s2_blog_" : "") + document.forms["artform"].elements["page[id]"].value);');
+Hooks.add('fn_save_article_end', 's2_search.refresh_index(sAction, document.forms["artform"].elements["page[id]"].value);');
