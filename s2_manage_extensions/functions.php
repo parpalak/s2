77c79d440a37d732032790fa7656170f7e624739
Roman <roman@written.ru>
2012-03-14 07:55:29 +0000

    s2_manage_extensions: Updated copyright year.

--- s2_manage_extensions/functions.php
+++ s2_manage_extensions/functions.php
@@ -4,7 +4,7 @@
  *
  * Server-side functions.
  *
- * @copyright (C) 2009-2011 Roman Parpalak
+ * @copyright (C) 2009-2012 Roman Parpalak
  * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
  * @package s2_manage_extensions
  */
