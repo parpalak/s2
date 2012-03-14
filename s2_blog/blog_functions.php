e0e5b6ab426da4a79dce8371478ee165cc9db5fe
Roman <roman@written.ru>
2012-03-14 07:49:37 +0000

    s2_blog: Updated copyright year.

--- s2_blog/blog_functions.php
+++ s2_blog/blog_functions.php
@@ -2,7 +2,7 @@
 /**
  * Helper functions for blog pages
  *
- * @copyright (C) 2007-2011 Roman Parpalak
+ * @copyright (C) 2007-2012 Roman Parpalak
  * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
  * @package s2_blog
  */
