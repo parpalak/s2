912236cdc4d52bfe7eb022eb1fe24985b9a46fe9
Roman <roman@written.ru>
2012-04-10 11:41:41 +0000

    Minor code cleanup.

--- _include/comments.php
+++ _include/comments.php
@@ -86,12 +86,15 @@ function s2_comment_form ($id, $name = '', $email = '', $showmail = false, $subs
 						<input type="submit" name="preview" value="<?php echo $lang_common['Preview']; ?>" />
 					</p>
 				</form>
-				<script type="text/javascript">
-var a=<?php echo ($s - $add2); ?>;
-a=a<?php echo ($bbb ? '-' : '+'), $add1; ?>;
-document.getElementById("quest").value=parseInt(a)+<?php echo ($c + $add2); ?>;
-document.getElementById("qsp").style.display="none";
-				</script>
+<script type="text/javascript">
+(function () 
+{
+	var a=<?php echo ($s - $add2); ?>;
+	a=a<?php echo ($bbb ? '-' : '+'), $add1; ?>;
+	document.getElementById("quest").value=parseInt(a)+<?php echo ($c + $add2); ?>;
+	document.getElementById("qsp").style.display="none";
+}());
+</script>
 <?php
 
 	return ob_get_clean();
