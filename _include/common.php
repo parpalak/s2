bbd0c4814cf17bd75eb04bbcbcf2c8d6f1834725
Roman <roman@written.ru>
2012-04-11 16:10:18 +0000

    Updated S2 version from 1.0b2 to 1.0b3.

--- _include/common.php
+++ _include/common.php
@@ -10,7 +10,7 @@
 require S2_ROOT.'_include/functions.php';
 require S2_ROOT.'_include/utf8/utf8.php';
 
-define('S2_VERSION', '1.0b2');
+define('S2_VERSION', '1.0b3');
 
 // Uncomment these lines for debug
 //define('S2_DEBUG', 1);
