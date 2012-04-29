6a451a81e0592747fe2309c03299dd1de5acb068
Roman <roman@written.ru>
2012-04-29 06:59:54 +0000

    s2_latex: Added notification when the cache folder is not writable.

--- s2_latex/lang/English.php
+++ s2_latex/lang/English.php
@@ -4,4 +4,6 @@ $lang_s2_latex = array(
 
 'Comment syntax'			=> 'Latex formulae: <span>$$</span>f(x) = x^2-\sqrt{x}<span>$$</span> converts into $$f(x) = x^2-\sqrt{x}$$.<br />',
 
+'Cache not writable'		=> 'PHP scripts have no write permissions to the “%s” directory. Set proper permissions (e. g. 777) for proper functioning of the “s2_latex” extension.',
+
 );
