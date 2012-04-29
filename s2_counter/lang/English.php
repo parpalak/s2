771c025426c80742703ba1e7ca46d0fd05323703
Roman <roman@written.ru>
2012-04-29 07:06:50 +0000

    s2_counter: Added notification when the data folder is not writable.

---
+++ s2_counter/lang/English.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang_s2_counter = array(
+
+'Data folder not writable'	=> 'PHP scripts have no write permissions to the “%s” directory. Set write permissions (e. g. 777) for proper functioning of the “s2_counter” extension.',
+
+);
