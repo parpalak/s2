7c26ac930a2ce6fe201adc595472553526e2e594
Roman <roman@written.ru>
2013-01-11 15:15:23 +0000

    s2_tpl_edit: Updated copyright year.

--- s2_tpl_edit/admin.js
+++ s2_tpl_edit/admin.js
@@ -1,7 +1,7 @@
 /**
  * Loading and saving templates
  *
- * @copyright (C) 2012 Roman Parpalak
+ * @copyright (C) 2012-2013 Roman Parpalak
  * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
  * @package s2_tpl_edit
  */
