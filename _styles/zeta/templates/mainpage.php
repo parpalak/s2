23aebbb33b126f36be482c9f8cb4f76a4f629a91
Roman <roman@written.ru>
2013-02-02 20:10:01 +0000

    Added templates to zeta style.

---
+++ _styles/zeta/templates/mainpage.php
@@ -0,0 +1,40 @@
+<?php if (!defined('S2_ROOT')) die; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title><!-- s2_head_title --></title>
+<!-- s2_meta -->
+<!-- s2_styles -->
+<!-- s2_navigation_link -->
+<!-- s2_rss_link -->
+</head>
+
+<body class="mainpage">
+	<div id="crumbs"><!-- s2_crumbs --></div>
+<!-- s2_search_field -->
+	<div id="header"><!-- s2_site_title --></div>
+
+	<div id="center">
+		<div id="container">
+			<div id="content">
+				<!-- s2_blog_last_post -->
+				<!-- s2_last_articles -->
+				<!-- s2_text -->
+			</div>
+			<div id="menu">
+				<!-- s2_menu -->
+				<!-- s2_last_comments -->
+				<!-- s2_blog_last_comments -->
+			</div>
+			<div class="clearing"></div>
+		</div>
+<!-- s2_debug -->
+	</div>
+
+	<div id="footer">
+		<p id="queries"><!-- s2_querytime --></p>
+		<p id="copyright"><!-- s2_copyright --></p>
+	</div>
+</body>
+</html>
---
+++ _styles/zeta/templates/site.php
@@ -0,0 +1,44 @@
+<?php if (!defined('S2_ROOT')) die; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title><!-- s2_head_title --></title>
+<!-- s2_meta -->
+<!-- s2_styles -->
+<!-- s2_navigation_link -->
+<!-- s2_rss_link -->
+</head>
+
+<body class="site">
+	<div id="crumbs"><!-- s2_crumbs --></div>
+<!-- s2_search_field -->
+	<div id="header"><!-- s2_site_title --></div>
+
+	<div id="center">
+		<div id="container">
+			<div id="content">
+				<!-- s2_author -->
+				<!-- s2_title -->
+				<!-- s2_date -->
+				<!-- s2_text -->
+				<!-- s2_subarticles -->
+				<!-- s2_tags -->
+				<!-- s2_comments -->
+				<!-- s2_comment_form -->
+			</div>
+			<div id="menu">
+				<!-- s2_menu -->
+				<!-- s2_article_tags -->
+			</div>
+			<div class="clearing"></div>
+		</div>
+<!-- s2_debug -->
+	</div>
+
+	<div id="footer">
+		<p id="queries"><!-- s2_querytime --></p>
+		<p id="copyright"><!-- s2_copyright --></p>
+	</div>
+</body>
+</html>
