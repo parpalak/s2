3d7696a70df938ce22838a51e112b8fa68700eaa
Roman <roman@written.ru>
2013-10-03 17:28:35 +0000

    s2_page_blocks: Added block formatting.

--- s2_page_blocks/lang/English.php
+++ s2_page_blocks/lang/English.php
@@ -4,6 +4,6 @@ $lang_s2_page_blocks = array(
 
 	'Page blocks'				=> 'Page blocks',
 	'Page blocks setup'			=> 'Additional editor fields',
-	'Page blocks setup label'	=> 'One field per line. Specify comma-separated list of the placeholder name, editor label, “img” for pictures or input height for texts. Examples: “placeholder, New field, 3”, “placeholder2, Img block, img”. Then use &lt;-- placeholder --&gt; in templates.',
+	'Page blocks setup label'	=> 'One field per line. Specify comma-separated list of the placeholder name, editor label, “img” for pictures or input height for texts and optional fourth parameter for the placeholder formatting. Examples: “placeholder, New field, 3, &lt;em&gt;%s&lt;/em&gt;”, “placeholder2, Img block, img”. Then use &lt;-- placeholder --&gt; in templates.',
 
 );
