9899ea8c551498ca33e45f5e65066e49424d33ad
Roman <roman@written.ru>
2013-12-09 09:29:39 +0000

    Fixed cookie expiration time limit.

--- _admin/login.php
+++ _admin/login.php
@@ -32,6 +32,8 @@ function s2_setcookie($name, $value, $expire = 0, $path = null, $secure = null)
 	if ($secure === null)
 		$secure = defined('S2_FORCE_ADMIN_HTTPS');
 
+	$expire = min($expire, PHP_INT_MAX); // Now we do not deal with 2037 year problem :)
+
 	if (version_compare(PHP_VERSION, '5.2.0', '>='))
 		setcookie($name, $value, $expire, $path, null, $secure, true);
 	else
