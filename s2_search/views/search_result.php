1dcaeb3d827a4176e446da9d658c76d4dab4ad68
Roman Parpalak <roman@written.ru>
2018-01-21 15:39:04 +0300

    s2_blog: Fixed empty descriptions in template.

--- s2_search/views/search_result.php
+++ s2_search/views/search_result.php
@@ -15,7 +15,7 @@ $link_escaped = implode('/', $a);
 ?>
 <p>
 	<a class="title" href="<?php echo s2_htmlencode($url); ?>"><?php echo s2_htmlencode($title); ?></a><br />
-	<?php echo $descr; ?><br />
+	<?php echo trim($descr) ? $descr . '<br />' : '';  ?>
 	<small class="stuff">
 		<a class="url" href="<?php echo s2_htmlencode($url); ?>"><?php echo (s2_abs_link($link_escaped)); ?></a>
 		<?php if (!empty($time)) echo ' &mdash; ', s2_date($time); ?>
